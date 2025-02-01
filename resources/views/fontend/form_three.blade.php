29ff55842a39a6f9d43a972ee6d102319240cb7f
Goni <gonialsayed1997@gmail.com>
2025-02-01 18:32:41 +0600

    live data show

--- resources/views/fontend/form_three.blade.php
+++ resources/views/fontend/form_three.blade.php
@@ -20,36 +20,73 @@
         <div class="container py-5">
             <div class="row gy-4">
                 <!-- Green Form -->
-                <div class="col-md-3"></div>
                 <div class="col-md-6">
                     <div class="p-4 form-green rounded">
                         <h4 class="text-success">Green Form</h4>
-                        <form action="{{route('register.info')}}" method="POST">
-                            @csrf;
-                            <input type="hidden" id="user_id" name="user_id">
+                        <form id="myForm" action="{{route('register.info')}}" method="POST">
+                            @csrf
+                
                             <div class="mb-3">
-                                <label for="emailYellow" class="form-label">First Name</label>
-                                <input type="text" class="form-control" name="fast_name" id="emailYellow" placeholder="Enter First Name">
+                                <label class="form-label">First Name</label>
+                                <input type="text" class="form-control" name="fast_name" id="fast_name" placeholder="Enter First Name">
                             </div>
                             <div class="mb-3">
-                                <label for="emailYellow" class="form-label">Last Name</label>
-                                <input type="text" class="form-control" name="last_name" id="emailYellow" placeholder="Enter Last Name">
+                                <label class="form-label">Last Name</label>
+                                <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Enter Last Name">
                             </div>
                             <div class="mb-3">
-                                <label for="emailRed" class="form-label">Father's Name</label>
-                                <input type="text" class="form-control" name="father_name" id="emailRed" placeholder="Enter Father's Name">
+                                <label class="form-label">Father's Name</label>
+                                <input type="text" class="form-control" name="father_name" id="father_name" placeholder="Enter Father's Name">
                             </div>
                             <div class="mb-3">
-                                <label for="passwordRed" class="form-label">Mother's Name</label>
-                                <input type="text" class="form-control" name="mother_name" id="passwordRed" placeholder="Enter Mother's Name">
+                                <label class="form-label">Mother's Name</label>
+                                <input type="text" class="form-control" name="mother_name" id="mother_name" placeholder="Enter Mother's Name">
                             </div>
                             <button type="submit" class="btn btn-danger">Submit</button>
                         </form>
                     </div>
                 </div>
+                <div class="col-md-6">
+                    <div class="p-4 form-red rounded">
+                        <h4 class="text-danger">Live Input Display</h4>
+                
+                        <div class="mb-3">
+                            <label class="form-label">First Name</label>
+                            <input type="text" class="form-control" id="display_fast_name" placeholder="First Name" readonly>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Last Name</label>
+                            <input type="text" class="form-control" id="display_last_name" placeholder="Last Name" readonly>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Father's Name</label>
+                            <input type="text" class="form-control" id="display_father_name" placeholder="Father's Name" readonly>
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Mother's Name</label>
+                            <input type="text" class="form-control" id="display_mother_name" placeholder="Mother's Name" readonly>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
+        <script>
+            document.addEventListener("DOMContentLoaded", function () {
+                // Get all input fields from the form
+                let inputs = document.querySelectorAll("#myForm input");
+        
+                inputs.forEach(function (input) {
+                    input.addEventListener("input", function () {
+                        // Find the corresponding output field in the second div
+                        let outputField = document.querySelector(`#display_${this.id}`);
+                        if (outputField) {
+                            outputField.value = this.value;
+                        }
+                    });
+                });
+            });
+        </script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     </body>
     </html>
